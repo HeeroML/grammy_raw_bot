85b5c7233116c48c7f4f7913f2fa4c3737b65fca
Heero <42873000+HeeroML@users.noreply.github.com>
2022-07-26 11:22:21 +0200

    Updated Used libs

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/grammy@v1.6.1/mod.ts";
+export * from "https://deno.land/x/grammy@v1.9.2/mod.ts";
 export { serve } from "https://deno.land/std@0.120.0/http/server.ts";
-export { escapeHtml } from "https://deno.land/x/escape@1.4.0/mod.ts";
-export { apiThrottler } from "https://deno.land/x/grammy_transformer_throttler@v1.0.1/mod.ts";
+export { escapeHtml } from "https://deno.land/x/escape@1.4.2/mod.ts";
+export { apiThrottler } from "https://deno.land/x/grammy_transformer_throttler@v1.1.2/mod.ts";
