7425fe2a8ee7e8b8536630dab6e0794ee572e55a
Heero <42873000+HeeroML@users.noreply.github.com>
2025-02-25 00:47:18 +0100

    deps.ts aktualisieren
    New functions preparation

--- deps.ts
+++ deps.ts
@@ -1,4 +1,19 @@
-export * from "https://deno.land/x/grammy@v1.30.0/mod.ts";
+export * from "https://deno.land/x/grammy@v1.35.0/mod.ts";
 export { serve } from "https://deno.land/std/http/server.ts";
 export { escapeHtml } from "https://deno.land/x/escape@1.4.2/mod.ts";
 export { apiThrottler } from 'https://lib.deno.dev/x/grammy_transformer_throttler@1/mod.ts';
+
+// Session related exports
+export { 
+  type Context as BaseContext,
+  type SessionFlavor, 
+  session,
+  type SessionData
+} from "https://deno.land/x/grammy@v1.35.0/mod.ts";
+export { freeStorage } from "https://deno.land/x/grammy_storages@v2.3.0/free/src/mod.ts";
+
+// For the inline keyboards
+export { 
+  InlineKeyboard, 
+  Keyboard 
+} from "https://deno.land/x/grammy@v1.35.0/mod.ts";
